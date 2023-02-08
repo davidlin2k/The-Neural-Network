8245e03b0e6ea5c84a205372aafd4f9284f583f8
David Lin <davidzy.lin@mail.utoronto.ca>
2023-02-07 21:11:18 -0500

    Initial Commit

---
+++ The Neural Network/The_Neural_NetworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_Neural_NetworkApp.swift
+//  The Neural Network
+//
+//  Created by David Lin on 2023-02-07.
+//
+
+import SwiftUI
+
+@main
+struct The_Neural_NetworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
