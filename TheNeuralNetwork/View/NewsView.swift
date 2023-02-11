627f37fc0e3a16adad860ba9309c8d1891177d02
David Lin <davidzylin@outlook.com>
2023-02-10 23:26:11 -0500

    Added country selector

--- TheNeuralNetwork/View/NewsView.swift
+++ TheNeuralNetwork/View/NewsView.swift
@@ -9,25 +9,45 @@ import SwiftUI
 
 struct NewsView: View {
     @EnvironmentObject private var newsModel: NewsModel
+    @Environment(\.colorScheme) var colorScheme: ColorScheme
     
     var body: some View {
-        List(self.newsModel.articles) { article in
-            NewsRow(article: article)
-                .fixedSize(horizontal: false, vertical: true)
-                .listRowSeparator(.hidden)
-                .listRowBackground(
-                    RoundedRectangle(cornerRadius: 5)
-                        .background(.clear)
-                        .foregroundColor(.white)
-                        .padding(
-                            EdgeInsets(
-                                top: 3,
-                                leading: 5,
-                                bottom: 3,
-                                trailing: 5
+    
+        VStack {
+            Picker("Select Country", selection: $newsModel.selection) {
+                ForEach(NewsModel.Country.allCases, id: \.self) { value in
+                    Text(value.description).tag(value)
+                            }
+                        }
+            .pickerStyle(.menu)
+            
+            List(self.newsModel.articles) { article in
+                NewsRow(article: article)
+                    .fixedSize(horizontal: false, vertical: true)
+                    .listRowSeparator(.hidden)
+                    .listRowBackground(
+                        RoundedRectangle(cornerRadius: 5)
+                            .background(.clear)
+                            .foregroundColor(colorScheme == .dark ? Color(UIColor.systemGray6) : .white)
+                            .padding(
+                                EdgeInsets(
+                                    top: 3,
+                                    leading: 5,
+                                    bottom: 5,
+                                    trailing: 3
+                                )
                             )
-                        )
-                )
+                    )
+                    .padding(
+                        EdgeInsets(
+                        top: 5,
+                        leading: 0,
+                        bottom: 5,
+                        trailing: 0
+                    ))
+            }.refreshable {
+                newsModel.loadHeadlines(country: newsModel.selection.rawValue)
+            }
         }
     }
 }
